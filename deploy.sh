56a91266f6e055b3700b1787452c0e9a17b84c7a
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2021-01-23 19:18:48 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,26 @@
 #!/bin/bash
 
-# ssh key
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-curl https://stuff.hostinguk.net/adg/pi/arc/key.txt > ~/.ssh/authorized_keys
-
-# SSH to keys only and port 343
-curl https://stuff.hostinguk.net/adg/pi/arc/sshd_config > /etc/ssh/sshd_config
-
-#Bashrc
-curl https://stuff.hostinguk.net/adg/pi/arc/bashrc >> ~/.bashrc
-
 # Install packages
 apt install dnsutils whois ipcalc -y
 
+# ssh key
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/key.txt > ~/.ssh/authorized_keys
+
+# SSH to keys only and port 343
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/sshd_config > /etc/ssh/sshd_config
+
+#Bashrc
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/bashrc >> ~/.bashrc
+
 #Set hostname
 hostname arc.ioserver.co.uk
+
+#Hosts
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/hosts >> /etc/hosts
+
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/rc.local >> /etc/rc.local
 
 #Deletes Pi User
 userdel -r -f pi
