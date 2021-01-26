715d6ebf0f78f5ee9fc7fa65289d5bebb482ba66
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2021-01-26 08:46:44 +0000

    Create motd.sh

---
+++ motd.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read one five fifteen rest < /proc/loadavg
+upSeconds="$(/usr/bin/cut -d. -f1 /proc/uptime)"
+
+printf "$(tput setaf 2)
+   .~~.   .~~.    $(date +"%A, %d.%m.%Y, %R:%S")
+  '. \ ' ' / .'   $(uname -srmo)$(tput setaf 1)
+   .~ .~~~..~.    Uptime...............: $(printf "%d days, %02d:%02d:%02d" "$((${upSeconds}/86400))" "$((${upSeconds}/3600%24))" "$((${upSeconds}/60%60))" "$((${upSeconds}%60))")
+  : .~.'~'.~. :   Memory...............: $(($(cat /proc/meminfo | grep MemAvailable | awk {'print $2'}) / 1024))M (Available) / $(($(cat /proc/meminfo | grep MemTotal | awk {'print $2'}) / 1024))M (Total)
+ ~ (   ) (   ) ~  Free Disk Space......: $(df -h | grep -E '^/dev/root' | awk '{ print $4 }') on /dev/root
+( : '~'.~.'~' : ) Running Processes....: $(ps ax | wc -l | tr -d " ")
+ ~ .~ (   ) ~. ~  Load Averages........: ${one}, ${five}, ${fifteen} (1, 5, 15 min)
+  (  : '~' :  )   Temperature..........: $(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")  C
+   '~ .~~~. ~'    Public IP Address....: $(dig +short myip.opendns.com @resolver1.opendns.com)
+       '~'
+\n$(tput sgr0)"
