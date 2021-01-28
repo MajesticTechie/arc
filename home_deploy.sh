77d5bcc217c57a0f54f447c1016dbbf506a33879
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2021-01-28 22:24:03 +0000

    Create home_deploy.sh

---
+++ home_deploy.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#Update
+apt-get update && apt-get upgrade -y
+
+# Install packages
+apt install dnsutils whois git ipcalc -y
+
+# ssh key
+mkdir ~/.ssh
+chmod 700 ~/.ssh
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/key > ~/.ssh/authorized_keys
+
+# SSH config
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/sshd_config > /etc/ssh/sshd_config
+
+#Bashrc
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/bashrc >> ~/.bashrc
+
+#MOTD Banner
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/home_motd > /etc/motd
+
+#Set hostname
+echo "Enter your Hostname:"
+read host_name
+hostname $host_name
+echo $host_name > /etc/hostname
+
+#Install Speed test CLI
+sudo apt-get install gnupg1 apt-transport-https dirmngr
+export INSTALL_KEY=379CE192D401AB61
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys $INSTALL_KEY
+echo "deb https://ookla.bintray.com/debian generic main" | sudo tee  /etc/apt/sources.list.d/speedtest.list
+sudo apt-get update
+# Other non-official binaries will conflict with Speedtest CLI
+# Example how to remove using apt-get
+# sudo apt-get remove speedtest-cli
+sudo apt-get install speedtest
+
+#Deletes Pi User
+userdel -r -f pi
+
+#Scripts
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/rebootchecker.sh > /rebootchecker.sh
+curl https://raw.githubusercontent.com/MajesticTechie/CloudflareDynamicDNS/main/CloudflareDNS.sh /CloudflareDNS.sh
+curl https://raw.githubusercontent.com/MajesticTechie/arc/main/ipcheck.sh > /ipcheck.sh
+
+## Confirm Install is now finished
+echo "
+#####################################################
+!!!!!!!!!!!! Automated install finished !!!!!!!!!!!!!
+#####################################################
+"
