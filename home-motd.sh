fa9ec189cd437fc9b648748eca56b3bc71876141
MajesticTechie <42770127+MajesticTechie@users.noreply.github.com>
2024-07-21 22:06:10 +0100

    Update home-motd.sh
    Grouped system related stats together, got rid of hostname and added disk check on external USB.

--- home-motd.sh
+++ home-motd.sh
@@ -12,12 +12,12 @@ fi
 printf "$(tput setaf 2)
    .~~.   .~~.    $(date +"%A, %d.%m.%Y, %R:%S")
   '. \ ' ' / .'   $(uname -srmo)$(tput setaf 1)
-   .~ .~~~..~.    Hostname.............: $(hostname)
-  : .~.'~'.~. :   Uptime...............: $(printf "%d days, %02d:%02d:%02d" "$((${upSeconds}/86400))" "$((${upSeconds}/3600%24))" "$((${upSeconds}/60%60))" "$((${upSeconds}%60))")
- ~ (   ) (   ) ~  Memory...............: $(($(cat /proc/meminfo | grep MemAvailable | awk {'print $2'}) / 1024))M (Available) / $(($(cat /proc/meminfo | grep MemTotal | awk {'print $2'}) / 1024))M (Total)
-( : '~'.~.'~' : ) Root -  Disk Free....: $(df -h | grep -E '^/dev/root' | awk '{ print $4 }') on /dev/root
- ~ .~ (   ) ~. ~  USB - Disk Free......: $(df -h | grep -E '^/dev/sda1' | awk '{ print $4 }') on /dev/sda1
-  (  : '~' :  )   Load Averages........: ${one}, ${five}, ${fifteen}
-   '~ .~~~. ~'    Temperature..........: $(/opt/vc/bin/vcgencmd measure_temp | cut -c "6-9")  C
+   .~ .~~~..~.    Load Averages........: ${one}, ${five}, ${fifteen}
+  : .~.'~'.~. :   Memory...............: $(($(cat /proc/meminfo | grep MemAvailable | awk {'print $2'}) / 1024))M (Available) / $(($(cat /proc/meminfo | grep MemTotal | awk {'print $2'}) / 1024))M (Total)
+ ~ (   ) (   ) ~  Uptime...............: $(printf "%d days, %02d:%02d:%02d" "$((${upSeconds}/86400))" "$((${upSeconds}/3600%24))" "$((${upSeconds}/60%60))" "$((${upSeconds}%60))")
+( : '~'.~.'~' : ) Root - Disk Free.....: $(df -h | grep -E 'mmcblk0p2' | awk '{ print $4 }') on /dev/root
+ ~ .~ (   ) ~. ~  Raid - Disk Free.....: $(df -h | grep -E '^/dev/sda1' | awk '{ print $4 }') on /dev/sdd1
+  (  : '~' :  )   Running Processes....: $(ps ax | wc -l | tr -d " ")
+   '~ .~~~. ~'    Temperature..........: $(/usr/bin/vcgencmd measure_temp)
        '~'        Reboot Required......: $reboot
 \n$(tput sgr0)"
